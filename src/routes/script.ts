1e3e1d2d116728f645aea30ac5c94bc36d158ec5
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-03-19 12:11:29 -0300

    chores: add a test for the DB conection

--- src/routes/script.ts
+++ src/routes/script.ts
@@ -1,8 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import * as PageController from '../controllers/pageController';
+import { Contato } from '../models/Contato';
 
 const router = Router();
 
 router.get('/ping', PageController.ping);
+
+// ! Apenas um teste temporário
+router.get('/', async (req: Request, res: Response) => {
+  let contatos = await Contato.findAll();
+  res.send({contatos})
+})
 
 export default router;
