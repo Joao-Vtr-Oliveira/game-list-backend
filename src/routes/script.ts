cdd917a42b923975967fe8890ba08da8c9be26f2
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-03-19 14:50:54 -0300

    docs: remove the TODO comments

--- src/routes/script.ts
+++ src/routes/script.ts
@@ -5,12 +5,6 @@ const router = Router();
 
 router.get('/ping', PageController.ping);
 
-// TODO -> [X] Rota para ver todos os contatos
-// TODO -> [X] Rota para ver um contato
-// TODO -> [X] Rota para Adicionar novo contato
-// TODO -> [X] Rota para deletar contato
-// TODO -> [] Rota para editar contato
-
 router.get('/contacts', PageController.getAllContacts);
 router.post('/contacts', PageController.postContact);
 router.get('/contacts/:id', PageController.getContact);
