85fdb6531dde80d8a97c118b857c8f8455caf24a
João V <joao.vtr.oliveira@outlook.com>
2024-05-21 12:54:39 -0300

    chores: change the conection varr

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -3,12 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const postgresDB = process.env.POSTGRES_DB as string;
+const postgresUser = process.env.POSTGRES_USER as string;
+const postgresPassword = process.env.POSTGRES_PASSWORD as string;
+const postgresHost = process.env.POSTGRES_HOST as string;
+const postgresPort = 5432;
+
+
 export const sequelize = new Sequelize(
-  process.env.PG_DB as string,
-  process.env.PG_USER as string,
-  process.env.PG_PASSWORD as string,
+  postgresDB,
+  postgresUser,
+  postgresPassword,
   {
+    host: postgresHost || "localhost",
     dialect: 'postgres',
-    port: Number(process.env.PG_PORT as string),
+    port: postgresPort,
   }
-);
+); 
