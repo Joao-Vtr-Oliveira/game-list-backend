52331050dae49243b1e17d43e191b383a4525496
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-07-10 16:06:01 -0300

    chores: dix the sh

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -2,10 +2,11 @@
 set -e
 
 host="$1"
-shift
+port="$2"
+shift 2
 cmd="$@"
 
-until pg_isready -h "$host"; do
+until nc -z "$host" "$port"; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
