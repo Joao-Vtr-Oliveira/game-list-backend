985055d5a4c78002aebe144dbc776f75882ed876
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-07-10 15:29:18 -0300

    chores: add a function that cleans the DB

---
+++ src/utils/dbUtils.ts
@@ -0,0 +1,18 @@
+// dbUtils.ts
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function clearDatabase() {
+    try {
+        await prisma.game.deleteMany({});
+        await prisma.category.deleteMany({});
+        // Adicione outras tabelas que deseja limpar aqui
+        console.log('Banco de dados limpo com sucesso.');
+    } catch (error) {
+        console.error('Erro ao limpar o banco de dados:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
