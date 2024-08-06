ebbd10f6cf9ba368cdf91bb00f5a45e3da109c0e
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-08-06 18:03:29 -0300

    chores: add the config in jest file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  maxConcurrency: 1,
   preset: 'ts-jest',
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
